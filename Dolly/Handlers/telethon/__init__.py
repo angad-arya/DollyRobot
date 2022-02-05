b62b84f5ea5ae0dac84be57a15dd79e65048ca7a
angad-arya <angadarya833@gmail.com>
2022-02-05 22:11:27 +0530

    Dolly

--- Dolly/Handlers/telethon/__init__.py
+++
@@ -1,7 +0,0 @@
-from Yone import DEV_USERS, INSPECTOR, REQUESTER, telethn
-
-IMMUNE_USERS = INSPECTOR.union(REQUESTER).union(DEV_USERS)
-
-IMMUNE_USERS = (
-    list(INSPECTOR) + list(REQUESTER) + list(DEV_USERS)
-)
