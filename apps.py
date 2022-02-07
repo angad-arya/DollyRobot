cd8b6aa50eacc0a0ec0afb119900d176a582b761
angad-arya <98724317+angad-arya@users.noreply.github.com>
2022-02-07 17:18:25 +0530

    Update apps.py

--- apps.py
+++ apps.py
@@ -1,4 +1,4 @@
-from telegram.ext import Updater, Dispatcher
+from telegram.ext import Updater
 from telegram.ext import CommandHandler, MessageHandler, Filter
 import os
 TOKEN = os.environ.get("TELEGRAM_ID")
